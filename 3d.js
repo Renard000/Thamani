7cc8238a83e79c7b1147cd0aba84512c2d023cb1
renard02 <pflorwensky@gmail.com>
2024-06-16 13:03:46 -0400

    compilater

--- 3d.js
+++ 3d.js
@@ -1,6 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var THREE = require("three");
 var Effet3D = /** @class */ (function () {
-    function Effet3D(name) {
-        this.name = document.querySelector(name);
+    function Effet3D(selector) {
+        this.name = document.querySelector(selector);
+        this.scene = null;
+        this.camera = null;
+        this.forme = null;
+        this.material = null;
+        this.object = null;
     }
+    ;
+    Effet3D.prototype.createScene = function () {
+        var _a;
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        (_a = this.name) === null || _a === void 0 ? void 0 : _a.appendChild(renderer.domElement);
+    };
+    ;
+    Effet3D.prototype.createObject = function () {
+        var _a;
+        this.forme = new THREE.PlaneGeometry(2, 2, 2);
+        this.material = new THREE.MeshBasicMaterial({ color: 0xfff0, side: THREE.DoubleSide });
+        this.object = new THREE.Mesh(this.forme, this.material);
+        (_a = this.scene) === null || _a === void 0 ? void 0 : _a.add(this.object);
+    };
+    ;
     return Effet3D;
 }());
+var view3d = new Effet3D("#view3dmod");
+view3d.createScene();
+view3d.createObject();
--- 3d.ts
+++ 3d.ts
@@ -1,35 +1,39 @@
 import * as THREE from 'three';
 
 class Effet3D{
-    #name : HTMLElement | null;
-    #scene : THREE.Scene | null;
-    #camera : THREE.PerspectiveCamera | null;
-    #forme : THREE.PlaneGeometry | null;
-    #material : THREE.MeshBasicMaterial | null;
-    #object : THREE.Mesh | null;
+    name : HTMLElement | null;
+    scene : THREE.Scene | null;
+    camera : THREE.PerspectiveCamera | null;
+    forme : THREE.PlaneGeometry | null;
+    material : THREE.MeshBasicMaterial | null;
+    object : THREE.Mesh | null;
 
     constructor(selector: string){
-        this.#name= document.querySelector(selector);
-        this.#scene = null;
-        this.#camera = null;
-        this.#forme = null;
-        this.#material = null;
-        this.#object = null;
+        this.name= document.querySelector(selector);
+        this.scene = null;
+        this.camera = null;
+        this.forme = null;
+        this.material = null;
+        this.object = null;
     };
 
     createScene(): void {
-        this.#scene= new THREE.Scene();
-        this.#camera= new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+        this.scene= new THREE.Scene();
+        this.camera= new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
         const renderer= new THREE.WebGLRenderer();
         renderer.setSize(window.innerWidth, window.innerHeight);
-        this.#name?.appendChild(renderer.domElement);
+        this.name?.appendChild(renderer.domElement);
     };
 
     createObject(): void {
-        this.#forme = new THREE.PlaneGeometry(2,2,2);
-        this.#material = new THREE.MeshBasicMaterial({color:0xfff0, side: THREE.DoubleSide});
-        this.#object = new THREE.Mesh(this.#forme, this.#material);
-        this.#scene?.add(this.#object);
+        this.forme = new THREE.PlaneGeometry(2,2,2);
+        this.material = new THREE.MeshBasicMaterial({color:0xfff0, side: THREE.DoubleSide});
+        this.object = new THREE.Mesh(this.forme, this.material);
+        this.scene?.add(this.object);
 
     };
 }
+
+const view3d: Effet3D = new Effet3D("#view3dmod");
+view3d.createScene();
+view3d.createObject();
