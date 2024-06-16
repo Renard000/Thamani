37a315dce2cf3be19a592bb6b3b82ac41fe19586
renard02 <pflorwensky@gmail.com>
2024-06-16 11:23:55 -0400

    initial dependency three

--- 3d.ts
+++ 3d.ts
@@ -1,22 +1,35 @@
 import * as THREE from 'three';
 
 class Effet3D{
-    name : HTMLElement | null;
-    scene : object | null;
-    camera :  object | null;
+    #name : HTMLElement | null;
+    #scene : THREE.Scene | null;
+    #camera : THREE.PerspectiveCamera | null;
+    #forme : THREE.PlaneGeometry | null;
+    #material : THREE.MeshBasicMaterial | null;
+    #object : THREE.Mesh | null;
+
     constructor(selector: string){
-        this.name= document.querySelector(selector);
+        this.#name= document.querySelector(selector);
+        this.#scene = null;
+        this.#camera = null;
+        this.#forme = null;
+        this.#material = null;
+        this.#object = null;
     };
 
     createScene(): void {
-        this.scene= new THREE.Scene();
-        this.camera= new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-        const renderer= new THREE.WEBLGRender();
+        this.#scene= new THREE.Scene();
+        this.#camera= new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+        const renderer= new THREE.WebGLRenderer();
         renderer.setSize(window.innerWidth, window.innerHeight);
-        document.appendChild(renderer.domElement);
+        this.#name?.appendChild(renderer.domElement);
     };
 
-    createObject(){
+    createObject(): void {
+        this.#forme = new THREE.PlaneGeometry(2,2,2);
+        this.#material = new THREE.MeshBasicMaterial({color:0xfff0, side: THREE.DoubleSide});
+        this.#object = new THREE.Mesh(this.#forme, this.#material);
+        this.#scene?.add(this.#object);
 
     };
 }
