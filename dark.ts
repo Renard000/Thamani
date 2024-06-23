a498e8a387f28c01769813a023459896f990ce67
renard02 <pflorwensky@gmail.com>
2024-06-23 19:32:38 -0400

    new update

---
+++ dark.ts
@@ -0,0 +1,21 @@
+class DArk{
+    body :HTMLElement | null;
+    text : HTMLElement | null;
+
+
+    constructor(){
+        this.body =document.querySelector("body");
+        this.text = document.querySelector("")
+        
+    }
+
+    enableDarkMode():void {
+
+    }
+    
+    disableDarkMode():void {
+
+    }
+    
+
+}
